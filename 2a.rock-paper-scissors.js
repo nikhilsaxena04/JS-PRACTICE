2d2f6d2aecc527a9bee24bc20bff3a631331e4fb
Nikhil Saxena <myemailnikhilsaxena@gmail.com>
2025-02-09 01:50:10 +0530

    added css

---
+++ 2a.rock-paper-scissors.js
@@ -0,0 +1,71 @@
+const score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+// || default operator, if left side is falsy, output right side
+updateScoreElement();
+
+// const score = {
+//         wins: 0,
+//         losses: 0,
+//         ties: 0
+//     };
+
+// console.log(JSON.parse(localStorage.getItem('score')));
+// //retrieves scores from local storage
+
+
+    //     //console.log(JSON.stringify(score)); //shows results in console
+    //     // console.log(JSON.parse(JSON.stringify(score)));
+    //     //score is an js object and can be converted to json object using JSON.stringify as strings, function can't be converted to json object
+    //     //JSON.parse(JSON.stringify(score)) is used to convert json object to js object
+
+function playGame(playerMove){
+    const moves = ['rock', 'paper', 'scissors'];
+    const computerMove = moves[Math.floor(Math.random() * 3)];
+/*
+Math.random() generates a random decimal between 0 and 1.
+Multiplying by 3 (Math.random() * 3) gives a range between 0 and 2.999.
+Math.floor() rounds down to 0, 1, or 2
+*/
+
+    let result = '';
+    if(playerMove === computerMove){
+        result = 'tie!';
+        score.ties++;
+    } else if(
+        (playerMove === 'rock' && computerMove === 'scissors') ||
+        (playerMove === 'paper' && computerMove === 'rock') ||
+        (playerMove === 'scissors' && computerMove === 'paper')
+    ){
+        result = 'You win!';
+        score.wins++;
+    } else {
+        result = 'You lose!';
+        score.losses++;
+    };
+
+    localStorage.setItem('score', JSON.stringify(score));
+//saved scores in local storage
+
+    updateScoreElement();
+
+    document.querySelector('.js-result').innerHTML = result
+
+    document.querySelector('.js-moves').innerHTML = `You 
+        <img src="images/${playerMove}-emoji.png" class="move-icon">
+        <img src="images/${computerMove}-emoji.png" class="move-icon">
+        Computer`;
+}
+
+// document.querySelector('.js-moves').innerHTML = `You picked ${playerMove} - Computer picked ${computerMove}`;
+
+
+
+//     alert(`You picked ${playerMove}, Computer picked ${computerMove}, ${result}, Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`);
+// }
+
+function updateScoreElement() {
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+};
