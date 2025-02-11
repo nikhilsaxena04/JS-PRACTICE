f789b86b195d2c9ab5c89cdbd0d756ee4df80d98
Nikhil Saxena <myemailnikhilsaxena@gmail.com>
2025-02-11 14:14:59 +0530

    use toFixed() to fix decimal in Prices

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
@@ -50,7 +50,7 @@ products.forEach((product) =>{
       </div>
 
       <div class="product-price">
-        $${product.priceInCents / 100}
+        $${(product.priceInCents / 100).toFixed(2)}
       </div>
 
       <div class="product-quantity-container">
