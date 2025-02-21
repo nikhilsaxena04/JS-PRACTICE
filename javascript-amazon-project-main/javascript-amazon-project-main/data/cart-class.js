bb7d2ae8964546ec0f9e2e6db4e5fc043b7cb58a
Nikhil Saxena <myemailnikhilsaxena@gmail.com>
2025-02-21 23:57:48 +0530

    created class version of cart

---
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/cart-class.js
@@ -0,0 +1,94 @@
+/*
+-classes are object generators/constructors
+-they have extra features, put setup code to make code cleaner
+-after we crete the object, we can run it in constructor to setup the object
+*/
+class Cart {
+  cartItems;
+  localStorageKey;
+
+  constructor(localStorageKey) { /*setup code*/
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  } // this points to object we generate
+
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    
+    if (!this.cartItems) {
+      this.cartItems = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionsId: '1'
+      }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionsId: '2'
+      }];
+    }
+  }
+
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+
+  addToCart(productId) {
+    let matchingItem;
+  
+      this.cartItems.find((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+  
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        this.cartItems.push({
+          productId: productId,
+          quantity : 1,
+          deliveryOptionsId: '1'
+      });
+    }
+
+    this.saveToStorage();
+  }
+
+
+  removeFromCart(productId) {
+    const newCart = [];
+    this.cartItems.forEach((cartItem) => {
+      if (productId !== cartItem.productId) {
+        newCart.push(cartItem);
+      }
+    });
+    this.cartItems = newCart;
+  
+    this.saveToStorage();
+  }
+
+
+  updateDeliveryOption(productId, deliveryOptionsId) {  
+    let matchingItem;
+  
+    this.cartItems.find((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+  
+    matchingItem.deliveryOptionsId = deliveryOptionsId;
+    this.saveToStorage();
+  }
+};
+
+const cart = new Cart('cart-oop');
+const cart2 = new Cart('cart-2');
+ /*each object we generate is called instance of the class*/
+
+console.log(cart);
+console.log(cart2);
+//console.log(cart2 instanceof Cart);
---
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/utils/amazon.js
@@ -0,0 +1,100 @@
+import {cart, addToCart} from '../../data/cart.js';
+import {products} from '../../data/products.js'
+import { formatCurrency } from './money.js';
+//loading products from product.js
+
+let productsHTML = ''; //accumulator
+
+//generate html for every product
+  //console.log(html)
+products.forEach((product) =>{
+  //const html =
+  productsHTML +=
+      `<div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button 
+        class="add-to-cart-button button-primary
+        js-add-to-cart"
+
+        data-product-id="${product.id}">
+
+          Add to Cart
+      </button>
+    </div>`
+});
+//console.log(productsHTML);
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+      cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+          //console.log(cartQuantity)
+          // console.log(cart);
+          //calculating total quantity of the cart
+}
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      //console.log("added product");
+      //console.log(button.dataset.productName);
+      const productId = button.dataset.productId;
+      /*
+          HTML uses kebab-case (data-product-name).
+          JavaScript automatically converts it to camelCase (productName).
+          dataset is an Object that Stores data- Attributes
+          JavaScript provides element.dataset, which automatically converts data- attributes into properties inside an object
+      */
+      addToCart(productId);
+      updateCartQuantity();
+  });
+});
